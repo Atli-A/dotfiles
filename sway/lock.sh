6e5185e2e4bf87fae823c4d98dc40dc2457a9e03
Atli-A <atli.nikulas@gmail.com>
2021-04-02 12:54:43 -0700

    epdated

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
-swaylock --indicator --clock --indicator-radius 200 --indicator-thickness 5 --fade-in 0.5 -c 000000 --text-color ffffff --text-clear-color ffffff --inside-clear-color 11111100 --line-clear-color 00000000 --grace 1
+swaylock --indicator --clock --indicator-radius 300 --indicator-thickness 5 --fade-in 0.5 --text-color ffffff --text-clear-color ffffff --inside-clear-color 11111100 --line-clear-color 00000000 --grace 1 --effect-blur 10x10 -S --effect-greyscale
+
 
 
