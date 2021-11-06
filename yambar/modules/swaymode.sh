44ad3f557fae882a41ec5b4e1655b0dfcd86ac29
Atli-A <atli.nikulas@gmail.com>
2021-11-06 00:12:42 -0700

    Changed style and  improved swaymode.sh in yambar.

--- yambar/modules/swaymode.sh
+++ yambar/modules/swaymode.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 while true; do
-	Y=$(swaymsg -t get_binding_state | jq -r '.name')
+	Y="$(swaymsg -t get_binding_state | jq -r '.name')"
+	if [ "$Y" = "default" ]; then
+		Y=""
+	fi
 	X="swaymode|string|$Y"
-	echo $X 
+	echo $X
 	echo ""
 	sleep $1
 done
